c1c9f31043d5b01c2966aa63ee6588cb393068da
mk10969 <mk10969@gmail.com>
2020-01-31 01:06:32 +0900

    metadata 作成task 追加

---
+++ uma-cloud-stream-source-jvLink-setup/src/test/java/org/uma/cloud/stream/source/TestUsageTest.java
@@ -0,0 +1,15 @@
+package org.uma.cloud.stream.source;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class TestUsageTest {
+
+    @Test
+    void test() {
+        assertEquals(1, 1);
+    }
+
+}
