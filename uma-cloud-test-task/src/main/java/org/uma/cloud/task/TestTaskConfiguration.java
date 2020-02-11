166f20702ce5673aa00a15552ba8fd960e67dd63
mk10969 <mk10969@gmail.com>
2020-02-12 01:55:40 +0900

    test-task OK

--- uma-cloud-test-task/src/main/java/org/uma/cloud/task/TestTaskConfiguration.java
+++ uma-cloud-test-task/src/main/java/org/uma/cloud/task/TestTaskConfiguration.java
@@ -1,16 +1,13 @@
 package org.uma.cloud.task;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.task.configuration.EnableTask;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.client.WebClient;
 
 
-@Slf4j
 @EnableTask
 @Configuration
 @EnableConfigurationProperties(TestTaskProperties.class)
@@ -23,15 +20,17 @@ public class TestTaskConfiguration {
     @Bean
     public CommandLineRunner testTaskRunner() {
         return args -> {
-            WebClient webClient = WebClient.create(testTaskProperties.getRestUrl());
-            webClient.get().uri("/")
-                    .retrieve()
-                    .bodyToMono(String.class)
-                    .subscribe(
-                            i -> System.out.println(i),
-                            e -> new RuntimeException(e),
-                            () -> System.out.println("end")
-                    );
+            System.out.println("aaaaaaaaaaaaaa");
+
+//            WebClient webClient = WebClient.create(testTaskProperties.getRestUrl());
+//            webClient.get().uri("/")
+//                    .retrieve()
+//                    .bodyToMono(String.class)
+//                    .subscribe(
+//                            i -> System.out.println(i),
+//                            e -> new RuntimeException(e),
+//                            () -> System.out.println("end")
+//                    );
 
         };
     }
